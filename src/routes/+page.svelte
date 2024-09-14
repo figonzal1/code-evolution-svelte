aa3efc0784d498f34a22377677c15ab71be926f5
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-09-14 18:44:17 -0300

    svelte tutorial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-lg text-red-400 font-bold">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class="underline text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+	documentation
+</p>
