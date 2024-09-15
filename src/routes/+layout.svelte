1734fca0f95eed72ae4de096786bebbed015ad06
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-09-15 20:27:02 -0300

    route matchers & layouts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<header class="bg-slate-500 text-[#f4a261] text-2xl text-center p-10 w-full">Header</header>
+<slot />
+
+<footer class="bg-slate-500 text-[#f4a261] text-2xl text-center w-full p-10">Footer</footer>
