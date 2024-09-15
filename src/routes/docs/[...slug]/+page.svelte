3da321d0c195d7a1ffccd3f99c2dce7ad77e7da8
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-09-15 17:48:13 -0300

    route all

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+
+	console.log($page.params);
+
+	const slugArr = $page.params.slug.split('/');
+</script>
+
+{#if slugArr.length == 1}
+	<h1>View docs for feature '{slugArr[0]}'</h1>
+{:else if slugArr.length == 2}
+	<h1>View docs for feature '{slugArr[0]}' and concept '{slugArr[1]}'</h1>
+{/if}
+
+<h1>Docs home page</h1>
---
+++ src/routes/products/[productId]/review/[reviewId]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+
+    const { productId, reviewId} = $page.params;
+</script>
+
+<h1>Review id: {reviewId} of product {productId}</h1>
